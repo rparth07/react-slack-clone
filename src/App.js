b961d39fd578eb309bb7425d1ff5a0e4f7fc5944
Parth Raiyani <gamer369man@gmail.com>
2020-09-15 21:36:23 +0530

    Signin styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import SignIn from './SignIn';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-        Hello world
+class App extends Component {
+  state = { counter: 1 };
+  render() {
+    return (
+      <div>
         <SignIn />
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/SignIn.js
+++ src/SignIn.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 
 class SignIn extends Component {
   render() {
-    return <div>SignIn</div>;
+    return (
+      <div className="signin-form">
+        <h1>Sign In</h1>
+        <button>Sign in with Google</button>
+        <form>
+          <input name="email" />
+          <input name="password" />
+        </form>
+      </div>
+    );
   }
 }
-
 export default SignIn;
