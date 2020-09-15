1a1161106775b6cc1c371b2e19fb08887b36bb40
Parth Raiyani <gamer369man@gmail.com>
2020-09-15 23:11:56 +0530

    Create sidebar, MainContainer, Slack Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import SignIn from './SignIn';
 import './App.css';
 import { Component } from 'react';
-
+import { Switch, Route } from 'react-router-dom';
+import MainContainer from './MainContainer';
+import Slack from './Slack';
+function Home() {
+  return <div> Home</div>;
+}
+function Some() {
+  return <div> Some</div>;
+}
 class App extends Component {
-  state = { counter: 1 };
   render() {
     return (
       <div>
-        <SignIn />
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/Home" component={Home} />
+          <Route exact path="/Slack" component={Slack} />
+        </Switch>
       </div>
     );
   }
